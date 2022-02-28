30f3bc79f5a19c204be6515df58cff4252a47c99
alecstephens <stephens.alec01@gmail.com>
2022-02-28 12:48:28 -0600

    finished the js

--- app.js
+++ app.js
@@ -17,5 +17,35 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/projects/:id', (req, res, next) => {
+    const {id} = req.params;
+    const project = projects[id];
 
+    if(project) {
+        res.render('project', {project})
+    } else {
+        next();
+    }
+});
+
+//listener
+app.listen(3000, () => {
+    console.log('The app is running on localhost:3000');
+});
+
+//error handlers
+app.use((req, res, next) => {
+    const err = new Error();
+    err.message = 'Page Not Found';
+    err.status = 404;
+    next(err);
+});
+
+app.use((req, res, next) => {
+    if(err.status === 404) {
+        res.status(404).render('not-found', err);
+    } else {
+        err.message = err.message || 'It looks like something went wrong with the server!';
+        res.status(err.status || 500);
+        res.render('error', err);
+    }
 });
